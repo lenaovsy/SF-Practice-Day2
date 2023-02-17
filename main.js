a5c92d9b84711cf079cffad11616cd7d1eb2a850
Elena Ovsy <leo.wrks@gmail.com>
2023-02-17 16:56:25 +0300

    SF-Practice-Day2
    Update

--- main.js
+++ main.js
@@ -13,12 +13,18 @@ MENU_LINKS.forEach(link => link.addEventListener('click', event => {
 const HERO_DOTS = document.querySelectorAll('.hero__dot');
 const HERO_IMAGE = document.querySelector('.hero__image');
 const HERO_TITLE = document.querySelector('.hero__title');
+const HERO_SUBTITLE = document.querySelector('.hero__subtitle');
 
 const HERO_TITLES = [
   'Разработано 2000+ приложений',
   'Бесплатные приложения',
   'Тысячи довольных клиентов',
 ];
+const HERO_SUBTITES = [
+  'Для вашего удобства мы разработали множество приложений для вашего бизнеса и команды, которые доступны в любое время суток',
+  'К любому сервисному обслуживанию мы подключим советы по управлению компанией совершенно бесплатно',
+  'Накопленный нами опыт позволяет предлагать клиентам обширный спектр качественных приложений и сервисов',
+]
 
 HERO_DOTS.forEach((dot, index) => dot.addEventListener('click', event => {
   HERO_DOTS.forEach(el => {
@@ -29,4 +35,5 @@ HERO_DOTS.forEach((dot, index) => dot.addEventListener('click', event => {
 
   HERO_IMAGE.src = 'assets/hero_' + (index + 1) + '.jpg';
   HERO_TITLE.textContent = HERO_TITLES[index];
+  HERO_SUBTITLE.textContent = HERO_SUBTITES[index];
 }))
